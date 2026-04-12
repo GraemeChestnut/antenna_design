ed34f0d9c6d2a6ebb873b8e089a5b5b55fa04922
GraemeChestnut <graemechesnut@gmail.com>
2026-04-12 09:01:09 -0700

    started to rebuild Antenna based on new number of segments. Allocation is completed, just need to do iterate math over each new segment

--- init_antenna.c
+++ init_antenna.c
@@ -25,8 +25,7 @@ Segment* init_antenna(int *n) {
         buffer[len] = '\0'; // null-terminate the string
         fclose(fp);
         
-
-        // parse the JSON data
+// parse the JSON data
         cJSON *json = cJSON_Parse(buffer);
 
         if (json == NULL) {
@@ -78,27 +77,44 @@ Segment* init_antenna(int *n) {
 
 Segment* init_sub_segments(Segment* Antenna, int num_segments, int frequency){
     
-    printf("this wave is running at %d, Hz", frequency);
     
-    float wavelength = C / frequency;
-    float sample_length = wavelength/10;
+    float wavelength = C / frequency; // wavelength of electron is c/f
+    float sample_length = wavelength/10; //sample_length is 1/10 of the wavelength
 
-    printf("%f\n", wavelength);
-    printf("%f\n", sample_length);
+    float magnitude;
+
+    int total_segments;
+    float num_sub_segments[num_segments];
+    Segment antenna_start_end_values[num_segments]; //storing start and end values in array in order to use them later for calacultaion when we overide previous values in Antenna
+    
 
     //find magnitude of line
     for(int i = 0; i < num_segments; ++i){
+        magnitude = sqrt(pow(Antenna[i].start_line[1] - Antenna[i].end_line[1],2) + pow((Antenna[i].start_line[2] - Antenna[i].end_line[2]), 2) + pow((Antenna[i].start_line[3] - Antenna[i].end_line[3]) ,2));
+
+        num_sub_segments[i] = ceil(magnitude / sample_length);
+        magnitude *= sample_length; //divide magnitude of line by SAMPLE_RATE
+ 
+        total_segments += (int)num_sub_segments[i];//FIND TOTAL NUMBER OF SEGMENTS NEEDED SO WE CAN REALLOC
         
-        
-        for(int k = 0; k < 3; ++k){
-           // Antenna[i].start_line[k]     
-               
+        for(int k; k < 3; ++k){
+            antenna_start_end_values[i].start_line[k] = Antenna[i].start_line[k];
+            antenna_start_end_values[i].end_line[k] = Antenna[i].end_line[k];
+        }
+     }
+    
+    Antenna = realloc(Antenna, (sizeof(Segment) * total_segments)); //reallocate memory for the total number of segments. This code looks like ass
+
+    //REBUILD ANTENNA WITH NEW LINES
+    
+    for(int i = 1; i < (num_segments+1); ++i){
+        for(int k = 0; k < num_sub_segments[i]; ++k){
+            for(int j = 0; j < 3; ++j){
+        //          Antenna[(i*k)].start_line[j] =  
+          //        Antenna[(i*k)].end_line[j] =
+            }
         }
     }
-
-    //divide magnitude of line by SAMPLE_RATE
-    //
-    //REBUILD ANTENNA WITH NEW LINES
 
     return Antenna;
 }
