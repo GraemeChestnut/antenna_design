95b92a8b681b6f16bcee737814c37571c96692ea
GraemeChestnut <graemechesnut@gmail.com>
2026-04-21 19:35:54 -0700

    have placeholder antenna sorted, math needed for final semgent initalization

--- init_antenna.c
+++ init_antenna.c
@@ -6,11 +6,10 @@
 #include "segment.h" 
 
 #define C 3e8
-#define SAMPLE_RATE 10
-
-Segment* init_sub_segments(Segment*, int num_segments, int frequency);
 
 Segment* init_antenna(int *n) {
+
+    const int sample_rate = 10;
 
     //JSON MUMBO JUMBO
         FILE *fp = fopen("antenna.json", "r");
@@ -44,80 +43,67 @@ Segment* init_antenna(int *n) {
 
     *n = number_of_segments->valueint;
 
-    Segment *Antenna = malloc((*n) * sizeof(Segment));
     cJSON *segments = cJSON_GetObjectItem(json, "segments");
 
+    //grab frequency data from JSON
     cJSON *frequency = cJSON_GetObjectItem(json, "frequency");
     int frequency_value = frequency->valueint; 
 
+    //SET ANTENNA_CONSTANTS
+    float wavelength = C / frequency_value; // wavelength of electron is c/f
+    float sample_length = wavelength/sample_rate; //sample_length is 1/10 of the wavelength
+    int total_segments;
+    float num_sub_segments[*n];
+    
+    float magnitude; 
+
+    Segment *Antenna = malloc((*n * sample_rate) * (sizeof(Segment))); //allocated alloted 
+    Segment plc_antenna[1]; //create placeholder values for json coordinates. Needed to find total number of sub segments within each segment, since you need the magnitude of each line.
+
     for(int i = 0; i < *n; ++i){
         cJSON *first_segment = cJSON_GetArrayItem(segments,  i);
-
-        for(int k = 0; k < 3; k++){
-            cJSON *start_line = cJSON_GetObjectItem(first_segment, "start_line");
-            cJSON *start_line_value = cJSON_GetArrayItem(start_line, k);
+        
+            for(int k = 0; k < 3; k++){
+                cJSON *start_line = cJSON_GetObjectItem(first_segment, "start_line");
+                cJSON *start_line_value = cJSON_GetArrayItem(start_line, k);
              
-            cJSON *end_line = cJSON_GetObjectItem(first_segment, "end_line");
-            cJSON *end_line_value = cJSON_GetArrayItem(end_line, k);
+                cJSON *end_line = cJSON_GetObjectItem(first_segment, "end_line");
+                cJSON *end_line_value = cJSON_GetArrayItem(end_line, k);
             
-            //printf("%f ", start_line_value->valuedouble);
+                //store i segment insdie of plc holder array
+                plc_antenna[i].start_line[k] = start_line_value->valuedouble;
+                plc_antenna[i].end_line[k] = end_line_value->valuedouble;
+            }
+            
+            //find magnitude of i segment
+            magnitude = sqrt(
+                pow(plc_antenna[0].start_line[0] - plc_antenna[0].end_line[0] , 2) +
+                pow(plc_antenna[0].start_line[1] - plc_antenna[0].end_line[1] , 2) +
+                pow(plc_antenna[0].start_line[2] - plc_antenna[0].end_line[2] , 2));
+            
+                
+            //compute number of necceasry sub segments inside of each segment, absed on the magnitune of that segmnet. ie, more sub_semgent sfor large line
+            int sub_segments = ceil( magnitude / sample_length);
 
-            Antenna[i].start_line[k] = start_line_value->valuedouble;
-            Antenna[i].end_line[k] = end_line_value->valuedouble;
+            //interate over number of sub_segments, and then knowning the sample_legnthfinally add true subsemgne initalization to main Antenna variabel
             
-        }
+            for(int m = 0; m < sub_segments; ++m){
+              //l looks too much like an i, we're using m cuz i can
+            
+              for(int o = 0; o < 3; ++o){
+                  //insert code calaculating individual start/end points for each new line segment, its place should be building off of the previous line semgnet; segment[m].start_line = segment[m-1].end_line
+                  //
+                  //
+              }
+            }
+                                        
+    
+
     }
 
     /*--------------------------------------------------------------------------------------------*/
-    
-    init_sub_segments(Antenna, *n, frequency_value);
-    
-    return Antenna;
+
+     return Antenna;
 }
-
-Segment* init_sub_segments(Segment* Antenna, int num_segments, int frequency){
-    
-    
-    float wavelength = C / frequency; // wavelength of electron is c/f
-    float sample_length = wavelength/10; //sample_length is 1/10 of the wavelength
-
-    float magnitude;
-
-    int total_segments;
-    float num_sub_segments[num_segments];
-    Segment antenna_start_end_values[num_segments]; //storing start and end values in array in order to use them later for calacultaion when we overide previous values in Antenna
-    
-
-    //find magnitude of line
-    for(int i = 0; i < num_segments; ++i){
-        magnitude = sqrt(pow(Antenna[i].start_line[1] - Antenna[i].end_line[1],2) + pow((Antenna[i].start_line[2] - Antenna[i].end_line[2]), 2) + pow((Antenna[i].start_line[3] - Antenna[i].end_line[3]) ,2));
-
-        num_sub_segments[i] = ceil(magnitude / sample_length);
-        magnitude *= sample_length; //divide magnitude of line by SAMPLE_RATE
- 
-        total_segments += (int)num_sub_segments[i];//FIND TOTAL NUMBER OF SEGMENTS NEEDED SO WE CAN REALLOC
-        
-        for(int k; k < 3; ++k){
-            antenna_start_end_values[i].start_line[k] = Antenna[i].start_line[k];
-            antenna_start_end_values[i].end_line[k] = Antenna[i].end_line[k];
-        }
-     }
-    
-    Antenna = realloc(Antenna, (sizeof(Segment) * total_segments)); //reallocate memory for the total number of segments. This code looks like ass
-
-    //REBUILD ANTENNA WITH NEW LINES
-    
-    for(int i = 1; i < (num_segments+1); ++i){
-        for(int k = 0; k < num_sub_segments[i]; ++k){
-            for(int j = 0; j < 3; ++j){
-        //          Antenna[(i*k)].start_line[j] =  
-          //        Antenna[(i*k)].end_line[j] =
-            }
-        }
-    }
-
-    return Antenna;
-}
-
 
 
