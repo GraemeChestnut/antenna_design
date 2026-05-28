a95a34ec665f3c777e306d8d0dd8c10110c1f6d6
GraemeChestnut <graemechesnut@gmail.com>
2026-05-28 08:37:23 -0700

    LU decomp functional. back/foward sub tbm

--- current.c
+++ current.c
@@ -16,10 +16,13 @@ double compute_R(float *midpoint_m, float *midpoint_n);
 
 void current_distribution(Segment *Antenna, int size, float *voltage, double wavelength){
       
-  double k = (2 * M_PI)/wavelength;
+  double beta = (2 * M_PI)/wavelength;
 
   double complex (*Z)[size] = malloc(size * size * sizeof(double complex));
-  printf("size = %d\n", size);
+  double complex (*upper)[size] = calloc(size * size, sizeof(double complex));
+  double complex (*lower)[size] = calloc(size * size, sizeof(double complex));
+  
+   printf("size = %d\n", size);
   printf("bytes needed = %zu\n", size * size * sizeof(double complex));
 
   if(Z == NULL){
@@ -38,38 +41,90 @@ void current_distribution(Segment *Antenna, int size, float *voltage, double wav
       }
       
       //computting R value
-      for(int o = 0; o < size; ++o){
+      for(int k = 0; k < size; ++k){
         
-        double r = 
-        sqrt(
-        pow(Antenna[i].midpoint[0] - Antenna[o].midpoint[0], 2) +
-        pow(Antenna[i].midpoint[1] - Antenna[o].midpoint[1], 2) +
-        pow(Antenna[i].midpoint[2] - Antenna[o].midpoint[2], 2)
-        );
-        
-        if (i == o){
-          Z[i][o] = greens_function(EPSILON, k);
-        }
-        else{
-        Z[i][o] = greens_function(r, k);
-        }
-
-printf("r[%d][%d] = %f   kR = %f   Z = %f + %fj\n", 
-            i, o, r, k*r, creal(Z[i][o]), cimag(Z[i][o]));
+          double r = 
+          sqrt(
+          pow(Antenna[i].midpoint[0] - Antenna[k].midpoint[0], 2) +
+          pow(Antenna[i].midpoint[1] - Antenna[k].midpoint[1], 2) +
+          pow(Antenna[i].midpoint[2] - Antenna[k].midpoint[2], 2)
+          );
+          
+          if (i == k){
+            Z[i][k] = greens_function(0.001, beta);
+          }
+          else{
+          Z[i][k] = greens_function(r, beta);
+          }
+          printf(" r[%d][%d] Z = %f + %f ", i, k, creal(Z[i][k]), cimag(Z[i][k]));
 
 
       }
+
+  }
+
+    for(int i = 0; i < size; ++i){
+    
+       // upper triangular
+      for(int k = i; k < size; k++){
+
+          double complex sum_U = 0 + 0.0*I;
+
+          for(int j = 0; j < i; j++){
+              //printf("%f + %fi * %f + %fi\n", creal(lower[i][j]), cimag(lower[i][j]), creal(upper[j][k]), cimag(upper[j][k]));
+              sum_U += (lower[i][j] * upper[j][k]);
+
+          }
+          //printf("%f + %fi\n", creal(sum_U), cimag(sum_U));
+          upper[i][k] = Z[i][k] - sum_U;
+      }
+      printf("\n");
+
+      // lower triangular
+      for(int k = i; k < size; k++){
+          if(i == k){
+              lower[i][i] = 1;
+          } else {
+
+              double complex sum_L = 0 + 0.0*I;
+
+              for(int j = 0; j < i; j++){
+                  sum_L += (lower[k][j] * upper[j][i]);
+                  //printf("%f + %fi", creal(sum_L), cimag(sum_L));
+              }
+              lower[k][i] = ((Z[k][i] - sum_L) / upper[i][i]);
+          }
+          //printf(" r[%d][%d] Z = %f + %f ", i, k, creal(lower[i][k]), cimag(lower[i][k]));
+          //printf(" r[%d][%d] Z = %f + %f ", i, k, creal(upper[i][k]), cimag(upper[i][k]));
+
+      }
+          
+      printf("\n");
     }
-  
+        
   free(Z);
-  printf("k = %f\n", k);
+  printf("k = %f\n", beta);
 printf("wavelength = %f\n", wavelength);
 
+
+
+for(int i = 0; i < size; i++) {
+    for(int j = 0; j < size; j++) {
+        printf("%f + %f ",
+               creal(upper[i][j]),
+               cimag(upper[i][j]));
+    }
+    printf("\n");
+}
+
+free(Z);
+free(lower);
+free(upper);
 }
 
 
 
 
-double complex greens_function(double r, double k){ //computing green's function with hemolt'z operator, which is used for 3D free space 
-    return (cexp(-I*k*r)/(4*M_PI*r));
+double complex greens_function(double r, double beta){ //computing green's function with hemolt'z operator, which is used for 3D free space 
+    return (cexp(-I*beta*r)/(4*M_PI*r));
 }
